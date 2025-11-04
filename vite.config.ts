d96fc0304ab1026932892eb87cdc911fe93ca757
Sofiane <45100006+sofiane51520@users.noreply.github.com>
2025-11-05 00:07:19 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: {
+        enabled: process.env.NODE_ENV === 'development', // Only enable in development
+        buildMode: true, // Ensure it runs during build in development
+      }}
+],
   base: '/gantt/', // 👈 important for GitHub Pages
 })
