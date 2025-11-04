ead12e992b7574948c581608964f524a2eea3807
sofiane <sofianebenazouzi@gmail.com>
2025-11-05 00:18:54 +0100

    ouafikljhqds

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    checker({
-      typescript: {
-        enabled: process.env.NODE_ENV === 'development', // Only enable in development
-        buildMode: true, // Ensure it runs during build in development
-      }})
-  ],
+  plugins: [react()],
   base: '/gantt/', // 👈 important for GitHub Pages
 })
